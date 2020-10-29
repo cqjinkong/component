bc70612974f0e13a73cb988107e86fd58b21ee05
gucao <280780363@qq.com>
2020-10-29 11:23:51 +0800

    add GetAliyunOssProvider

---
+++ src/Jinkong.AliyunOss/Extensions.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jinkong.AliyunOss
+{
+    public static class Extensions
+    {
+        public static IAliyunOssProvider? GetAliyunOssProvider(this IServiceProvider serviceProvider, string bucketName)
+        {
+            return serviceProvider.GetServices<IAliyunOssProvider>()
+                .FirstOrDefault(r => r.BucketName == bucketName);
+        }
+    }
+}
