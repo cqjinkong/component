ac6450360a88f18132fb7c6d2da613c48c441ad5
gucao <280780363@qq.com>
2020-10-29 15:29:07 +0800

    update shashlik version

--- src/Jinkong.AliyunOss/Extensions.cs
+++ src/Jinkong.AliyunOss/Extensions.cs
@@ -7,6 +7,12 @@ namespace Jinkong.AliyunOss
 {
     public static class Extensions
     {
+        /// <summary>
+        /// 根据存储桶名称获取oss提供类
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <param name="bucketName"></param>
+        /// <returns></returns>
         public static IAliyunOssProvider? GetAliyunOssProvider(this IServiceProvider serviceProvider, string bucketName)
         {
             return serviceProvider.GetServices<IAliyunOssProvider>()
