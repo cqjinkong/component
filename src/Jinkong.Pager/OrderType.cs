76d3a5d11685d97ff3edca8d3484f7c7e73d1196
gucao <280780363@qq.com>
2020-11-14 17:44:20 +0800

    add jinkong.pager

---
+++ src/Jinkong.Pager/OrderType.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+namespace Jinkong.Pager
+{
+    /// <summary>
+    /// 排序类型
+    /// </summary>
+    public enum OrderType
+    {
+        /// <summary>
+        /// 升序
+        /// </summary>
+        asc = 0,
+
+        /// <summary>
+        /// 降序
+        /// </summary>
+        desc = 1
+    }
+}
---
+++ src/Jinkong.Pager/PageInput.cs
@@ -0,0 +1,36 @@
+﻿// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
+// ReSharper disable UnusedMember.Global
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Jinkong.Pager
+{
+    /// <summary>
+    /// 分页输入
+    /// </summary>
+    public class PageInput
+    {
+        /// <summary>
+        /// 分页参数,默认1
+        /// </summary>
+        [Range(1, int.MaxValue, ErrorMessage = ("参数错误"))]
+        public int PageIndex { get; set; } = 1;
+
+        /// <summary>
+        /// 分页参数,默认20
+        /// </summary>
+        [Range(1, int.MaxValue, ErrorMessage = ("参数错误"))]
+        public int PageSize { get; set; } = 20;
+
+        /// <summary>
+        /// 排序字段
+        /// </summary>
+        public string OrderField { get; set; }
+
+        /// <summary>
+        /// 排序方式
+        /// </summary>
+        public OrderType OrderType { get; set; }
+    }
+}
